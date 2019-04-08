4eb0a8d2fce3d8f5cc38543e934678568f15a503
João Vitor <34198932+jvitorsl@users.noreply.github.com>
2019-04-08 18:39:14 -0300

    Importando código

---
+++ index.js
@@ -0,0 +1,7 @@
+window.onload = function page(){
+    aaa.onclick() = function musica(){
+        var fileVal=document.getElementById("aaa").value;
+        document.getElementById("mds").src=fileVal;
+        document.getElementById("tst").innerHTML=mds.src;
+    }
+}
